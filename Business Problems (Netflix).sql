281d3acc3fec08a6031fcf8aabb82ed0f77c7dd3
Priyanshi Negi <121029180+PriyanshiNegi01@users.noreply.github.com>
2024-12-23 23:52:23 +0530

    Business Problems

---
+++ Business Problems (Netflix).sql
@@ -0,0 +1,20 @@
+-- 15 Business Problems
+
+1. Count the number of Movies vs. TV Shows.
+2. Find the most common rating for Movies and TV Shows.
+3. List all Movies released in a specific year (e.g., 2020).
+4. Find the top 5 countries with the most content on Netflix.
+5. Identify the longest Movie.
+6. Find content added in the last 5 years.
+7. Find all Movies and TV Shows directed by 'Karan Johar'.
+8. List all TV Shows with more than 5 seasons.
+9. Count the number of content items in each genre.
+10. For each year, find the average number of content releases in India on Netflix.
+	- Return the top 5 years with the highest average content releases.
+11. List all Movies that are documentaries.
+12. Find all content items without a director.
+13. Count how many Movies actor 'Shahid Kapoor' appeared in during the last 10 years.
+14. Find the top 10 actors who have appeared in the highest number of Movies produced in India.
+15. Categorize the content based on the presence of the keywords 'kill' and 'violence' in the description field. 
+	- Label content containing these keywords as 'Bad' and all other content as 'Good'. 
+	- Count how many items fall into each category.
